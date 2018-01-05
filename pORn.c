307723218f2a75c67bd042f3d41764567c9f25ba
xdutsuay <33772826+xdutsuay@users.noreply.github.com>
2018-01-05 22:22:07 +0530

    Create pORn.c

---
+++ pORn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int x;
+printf(" enter a number range +-100000");
+scanf("%d",&x);
+if(x>100000)
+printf("enter again");
+else if(x<0)
+printf("Negative");
+else
+printf("positive");
+}
