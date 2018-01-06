201c353940ca897df974238683cffd4fa0f52936
xdutsuay <33772826+xdutsuay@users.noreply.github.com>
2018-01-07 04:46:29 +0530

    Create vorc.py

---
+++ vorc.py
@@ -0,0 +1,5 @@
+in=input("enter a charcter")
+if in=='a' or in=='e' or in=='i' or in=='o' or in=='u' or in=='A' or in=='E' or in=='I' or in=='O' or in=='U' :
+  print("vowel")
+else  
+  print("consonent");
